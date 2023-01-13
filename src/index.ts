e0a5360191b3ce04413809448c4b1f27414a769b
Justin Leider <justin@bondlink.com>
2023-01-13 11:16:55 -0500

    Fix formatting and linter warning

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import escapeStringRegexp from "escape-string-regexp";
+import escapeStringRegexp from 'escape-string-regexp';
 
-type Flags = "d" | "g" | "i" | "m" | "s" | "u" | "y";
+type Flags = 'd' | 'g' | 'i' | 'm' | 's' | 'u' | 'y';
 
-type OnlyAcceptedChars<AcceptedChars extends string, A extends string, Acc extends string = ""> =
-    A extends AcceptedChars
-    ? `${Acc}${A}`
-    : A extends `${AcceptedChars}${infer Tail}`
-    ? OnlyAcceptedChars<AcceptedChars, Tail, `${Acc}${AcceptedChars}`>
-    : never;
+type OnlyAcceptedChars<AcceptedChars extends string, A extends string, Acc extends string = ''> =
+  A extends AcceptedChars
+  ? `${Acc}${A}`
+  : A extends `${AcceptedChars}${infer Tail}`
+  ? OnlyAcceptedChars<AcceptedChars, Tail, `${Acc}${AcceptedChars}`>
+  : never;
 
 type Instances<
-    S extends string,
-    Query extends string,
-    Acc extends Query[] = []
+  S extends string,
+  Query extends string,
+  Acc extends Query[] = []
 > =
-    // edge case: if `S` isn't a string literal/string union just return `string[]`
-    string extends S ? string[] :
-    //  edge case: if the query isn't a string literal/string union return never;
-    string extends Query ? never :
-    // base case: return the instances we'e accumulated
-    S extends "" ? Acc :
-    // recursive case: adds current match to accumulator and recurses with tail
-    S extends `${string}${Query}${infer Rest}` ?
-    Instances<Rest, Query, [...Acc, Query]> :
-    Acc;
+  // edge case: if `S` isn't a string literal/string union just return `string[]`
+  string extends S ? string[] :
+  //  edge case: if the query isn't a string literal/string union return never;
+  string extends Query ? never :
+  // base case: return the instances we'e accumulated
+  S extends '' ? Acc :
+  // recursive case: adds current match to accumulator and recurses with tail
+  S extends `${string}${Query}${infer Rest}` ?
+  Instances<Rest, Query, [...Acc, Query]> :
+  Acc;
 
 type InstancesToStrings<A extends string[]> = [...{ [K in keyof A]: string }];
 
-export const regexp = <S extends string, F extends string>(r: S, flags?: OnlyAcceptedChars<Flags, F>) => (...strings: InstancesToStrings<Instances<S, "<_>">>): RegExp => {
-    // eslint-disable-next-line no-restricted-syntax
-    return new RegExp(r.replace(/<_>/g, () => escapeStringRegexp(strings.shift() || "")), flags);
+export const regexp = <S extends string, F extends string>(r: S, flags?: OnlyAcceptedChars<Flags, F>) => (...strings: InstancesToStrings<Instances<S, '<_>'>>): RegExp => {
+
+  return new RegExp(r.replace(/<_>/g, () => escapeStringRegexp(strings.shift() || '')), flags);
 };
 
-export const regexpSimple = <F extends string>(str: string, flags?: OnlyAcceptedChars<Flags, F>) => regexp("<_>", flags)(str);
+export const regexpSimple = <F extends string>(str: string, flags?: OnlyAcceptedChars<Flags, F>) => regexp('<_>', flags)(str);
